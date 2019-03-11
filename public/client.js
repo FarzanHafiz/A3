142f00d186aab602d4f9007679dc46cd7c0b0ecb
Farzan Hafiz <shahfarzan.alhafiz@ucalgary.ca>
2019-03-10 23:01:51 -0600

    almost

--- public/client.js
+++ public/client.js
@@ -6,8 +6,57 @@ $(function () {
       $('#m').val('');
       return false;
     });
+
     socket.on('chat message', function(msg){
       $('#messages').append($('<li>').text(msg));
       $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    //add user
+    socket.on('connect', function(){
+        socket.emit('adduser');
+        socket.emit('update userlist');
+    });
+
+    //update user list
+    socket.on('onlineUsers', function(msg){
+        $('#users').empty();
+
+        var message = "";
+        for (i = 0; i < msg.length; i += 2)
+        {
+            message += "<b>" + msg[i] + "</b>" + "<br/>"
+        }
+        $('#users').append($('<li>').html(message));
+    });
+
+    //display user list
+    socket.on('userNameTitle', function(msg){	
+        $('#userNameTitle').html(msg);
+    });
+
+    //display chat messages
+    socket.on('chat message', function(msg1, name, msg2, color){
+        var entireMessage;
+        name = name.fontcolor(color);
+        entireMessage = msg1 + name + msg2;
+        $('#messages').append($('<li>').html(entireMessage));
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    //bold own message
+    socket.on('bold message', function(msg1, name, msg2, color){
+        var completeMessage;
+        name = name.fontcolor(color);
+        completeMessage = "<b>" + msg1 + name + msg2 + "</b>";
+        $('#messages').append($('<li>').html(completeMessage));
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    //chat history displayed in italics font
+    socket.on('chat history', function(msg){
+        msg = "<i>" + msg + "</i>";
+        $('#messages').append($('<li>').html(msg));
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
     });
   });
